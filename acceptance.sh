0be6323833c6db2fb415c96715337539febabf88
James Eastham <j.eastham1993@gmail.com>
2021-07-28 23:16:30 +0100

    Update shell file paths

--- acceptance.sh
+++ acceptance.sh
@@ -1,4 +1,4 @@
-docker build -f .\src\JEasthamDev.Api\Dockerfile -t jeasthamdev-api .
+docker build -f ./src/JEasthamDev.Api/Dockerfile -t jeasthamdev-api .
 
 docker run -p 5000:80 -d jeasthamdev-api
 
