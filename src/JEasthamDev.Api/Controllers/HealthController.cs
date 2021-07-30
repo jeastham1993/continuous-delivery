697f6385845e8b1b1aab0b61675df0d80c3c69c2
James Eastham <dev@jameseastham.co.uk>
2021-07-30 22:12:35 +0100

    Add health check to API

---
+++ src/JEasthamDev.Api/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JEasthamDev.Api.Domain.Commands.CreateOrder;
+using JEasthamDev.Api.Domain.Entity;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JEasthamDev.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return this.Ok();
+        }
+    }
+}
